b5a40ad2467d5f5cf46bd9afa79250de598dfc0f
Rifa Safeer Shah <rifashah28@gmail.com>
2020-09-19 12:44:04 -0700

    Create StockNotification.cs

---
+++ labs/lab assignment 3/StockNotification.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stock
+{
+    public class StockNotification : EventArgs
+    {
+        public string StockName { get; set; }
+        public int CurrentValue { get; set; }
+        public int NumChanges { get; set; }
+
+        /// <summary>
+        ///     Stock notification attributes that are set and changed
+        /// </summary>
+        /// <param name="stockName">Name of stock</param>
+        /// <param name="currentValue">Current vallue of the stock</param>
+        /// <param name="numChanges">Number of changes the stock goes through</param>
+        public StockNotification(string stockName, int currentValue, int numChanges)
+        {
+            StockName = stockName;
+            CurrentValue = currentValue;
+            NumChanges = numChanges;
+        }
+    }
+}
